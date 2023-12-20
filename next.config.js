e49417468b44f10e119a212bc2aead0f177a1988
sathish <sathishchandran321@gmail.com>
2023-12-20 12:43:50 +0530

    commit

--- next.config.js
+++ next.config.js
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    basePath: process.env.NODE_ENV === 'laptop' ? '/laprent' : '',
+
+}
 
 module.exports = nextConfig
